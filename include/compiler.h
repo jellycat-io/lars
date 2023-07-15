8bc295bc5bd735d96dd35fcb6f4b2d9d8195343c
salty-max <max@jellycat.fr>
2023-07-15 20:32:12 +0200

    feat: implements parser/compiler foundations

---
+++ include/compiler.h
@@ -0,0 +1,8 @@
+#ifndef lars_compiler_h
+#define lars_compiler_h
+
+#include "chunk.h"
+
+bool compile(const char *source, Chunk *chunk);
+
+#endif
--- src/logger.c
+++ src/logger.c
@@ -11,3 +11,14 @@ void logWithColor(const char *color, const char *format, ...) {
 
   va_end(args);
 }
+
+void logError(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+
+  printf("%s", ANSI_COLOR_RED);
+  fprintf(stderr, format, args);
+  printf("%s", ANSI_COLOR_RESET);
+
+  va_end(args);
+}
